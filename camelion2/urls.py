f8c839b66b241620fd51ec21113a843b0099e2e1
NephygTdkingz <tdkingzict@gmail.com>
2024-04-26 17:02:37 +0100

    added static root and installed gunicorn

--- camelion2/urls.py
+++ camelion2/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('frontend/',include('frontend.urls', namespace='frontend') ),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
